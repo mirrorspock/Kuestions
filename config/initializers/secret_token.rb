c04879c8a06f67be61fb35facf842e7674383dd4
Kai-Arne Reiter <git@mirrorspock.com>
2013-12-08 14:06:25 +0100

    initialized

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kuestions::Application.config.secret_key_base = 'e23f3ea9dfa0325417ad4d09f1505861983142db216b57b9091184e50b4d3033d369252f5c2d171b19fa50fa8b1aa35faa2fe32a3d3a5a6ec6ca5a56c527316e'
